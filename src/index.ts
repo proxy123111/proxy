bda8c4a331e87703a00008d980b8456f99217cfb
YahiroRyo <honakafn@gmail.com>
2023-02-02 22:04:21 +0900

    console log headers

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ app.get("/", async (req: express.Request, res: express.Response) => {
     }
     
     const response = await fetch(`https://global.cainiao.com/global/detail.json?mailNos=${req.query.mailNos}&lang=en-US`)
+    console.log(response.headers)
     if (!response.headers.get('Content-Type') || response.headers.get('Content-Type')!.indexOf('text/html') !== -1) {
         res.status(429).send();
         return;
